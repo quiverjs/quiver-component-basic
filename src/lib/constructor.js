6ba8d8fe88d253b6690786cc4c877e0b94cf7adc
Soares Chen <soares.chen@gmail.com>
2015-08-08 01:15:14 +0800

    Add constructor functions.

---
+++ src/lib/constructor.js
@@ -0,0 +1,38 @@
+import {
+  implComponentConstructor, safeBuilder, safeHandler
+} from 'quiver-component-util'
+
+import {
+  StreamFilter, HttpFilter,
+  HandleableFilter,
+  HttpHandler, HttpHandlerBuilder,
+  SimpleHandler, SimpleHandlerBuilder,
+  StreamHandler, StreamHandlerBuilder
+} from './index'
+
+export const streamFilter = implComponentConstructor(
+  StreamFilter, 'streamFilterFn', safeBuilder)
+
+export const httpFilter = implComponentConstructor(
+  HttpFilter, 'httpFilterFn', safeBuilder)
+
+export const handleableFilter = implComponentConstructor(
+  HandleableFilter, 'handleableFilterFn', safeHandler)
+
+export const httpHandler = implComponentConstructor(
+  HttpHandler, 'httpHandlerFn', safeHandler)
+
+export const httpHandlerBuilder = implComponentConstructor(
+  HttpHandlerBuilder, 'httpHandlerBuilderFn', safeBuilder)
+
+export const simpleHandler = implComponentConstructor(
+  SimpleHandler, 'simpleHandlerFn', safeHandler)
+
+export const simpleHandlerBuilder = implComponentConstructor(
+  SimpleHandlerBuilder, 'simpleHandlerBuilderFn', safeBuilder)
+
+export const streamHandler = implComponentConstructor(
+  StreamHandler, 'streamHandlerFn', safeHandler)
+
+export const streamHandlerBuilder = implComponentConstructor(
+  StreamHandlerBuilder, 'streamHandlerBuilderFn', safeBuilder)
